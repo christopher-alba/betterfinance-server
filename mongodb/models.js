5dfbe87b0ac0fd6f6c156c7864902e3f66bc3552
christopher-alba <calb200@aucklanduni.ac.nz>
2021-11-29 20:16:43 +1300

    write models

--- mongodb/models.js
+++ mongodb/models.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose");
-const { userSchema } = require("./schema");
+const {
+  userSchema,
+  profileSchema,
+  incomeSchema,
+  expensesSchema,
+  goalsSchema,
+} = require("./schema");
 
 const User = mongoose.model("User", userSchema);
+const Profile = mongoose.model("Profile", profileSchema);
+const Income = mongoose.model("Income", incomeSchema);
+const Expense = mongoose.model("Expense", expensesSchema);
+const Goal = mongoose.model("Goal", goalsSchema);
 
 module.exports = {
   User,
+  Profile,
+  Income,
+  Expense,
+  Goal,
 };
