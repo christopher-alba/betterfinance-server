c2a38f3676b7698a4215018567389e348e4399a0
Christopher Alba <calb200@aucklanduni.ac.nz>
2022-04-15 11:18:33 +1200

    add check in backend for invalid date for goals

--- mongodb/db/goals.js
+++ mongodb/db/goals.js
@@ -1,55 +1,63 @@
 const { Goal } = require("../models");
 
 const createGoal = async (goalObj) => {
-    try {
-      const res = await Goal.create(goalObj.goal);
-      return res;
-    } catch (err) {
-      return new Error(err.message);
+  try {
+    if (
+      new Date(goalObj.goal.completionDate) <
+      new Date(Date.now() + 1000 * 60 * 60 * 24 * 2)
+    ) {
+      throw new Error(
+        "target completion date is invalid - set to the past, not future"
+      );
     }
-  };
-  
-  const updateGoal = async (goalObj, goalID) => {
-    try {
-      await Goal.updateOne({ _id: goalID }, goalObj);
-      const res = await Goal.findOne({ _id: goalID });
-      return res;
-    } catch (err) {
-      return new Error(err.message);
-    }
-  };
-  
-  const deleteGoal = async (goalID) => {
-    try {
-      const res = await Goal.deleteOne({ _id: goalID });
-      return res.deletedCount;
-    } catch (err) {
-      return new Error(err.message);
-    }
-  };
-  
-  const deleteAllUserGoals = async (profileID) => {
-    try {
-      const res = await Goal.deleteMany({ profileID: profileID });
-      return res.deletedCount;
-    } catch (err) {
-      return new Error(err.message);
-    }
-  };
-  
-  const getAllUserGoals = async (profileID) => {
-    try {
-      const res = await Goal.find({ profileID: profileID });
-      return res;
-    } catch (err) {
-      return new Error(err.message);
-    }
-  };
-  
-  module.exports = {
-    createGoal,
-    updateGoal,
-    deleteGoal,
-    deleteAllUserGoals,
-    getAllUserGoals,
-  };
+    const res = await Goal.create(goalObj.goal);
+    return res;
+  } catch (err) {
+    return new Error(err.message);
+  }
+};
+
+const updateGoal = async (goalObj, goalID) => {
+  try {
+    await Goal.updateOne({ _id: goalID }, goalObj);
+    const res = await Goal.findOne({ _id: goalID });
+    return res;
+  } catch (err) {
+    return new Error(err.message);
+  }
+};
+
+const deleteGoal = async (goalID) => {
+  try {
+    const res = await Goal.deleteOne({ _id: goalID });
+    return res.deletedCount;
+  } catch (err) {
+    return new Error(err.message);
+  }
+};
+
+const deleteAllUserGoals = async (profileID) => {
+  try {
+    const res = await Goal.deleteMany({ profileID: profileID });
+    return res.deletedCount;
+  } catch (err) {
+    return new Error(err.message);
+  }
+};
+
+const getAllUserGoals = async (profileID) => {
+  try {
+    const res = await Goal.find({ profileID: profileID });
+    return res;
+  } catch (err) {
+    return new Error(err.message);
+  }
+};
+
+module.exports = {
+  createGoal,
+  updateGoal,
+  deleteGoal,
+  deleteAllUserGoals,
+  getAllUserGoals,
+};
